6f5f3bf35c877742a509451903dd0faacc0ac624
kyeonkim <kyeonkim5021@gmail.com>
2024-09-30 17:03:41 +0900

    chore(#3): Gateway API 생성

---
+++ service/gateway/server/src/test/java/com/sparta/gateway/server/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.gateway.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
