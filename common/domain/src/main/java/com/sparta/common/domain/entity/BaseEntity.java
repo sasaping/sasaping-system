418c8d437daea82c6fb333a5d8ad254cdbfb1542
harper <35358294+yooyouny@users.noreply.github.com>
2024-09-30 15:31:49 +0900

    feat(#6): 공통모듈에 공통응답 객체들 추가 - 공통 커스텀 예외인 BusinessException - 공통 응답인 ApiResponse - Audit 속성을 정의한 BaseEntity

---
+++ common/domain/src/main/java/com/sparta/common/domain/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.sparta.common.domain.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+  @CreatedDate private LocalDateTime createdAt;
+
+  @CreatedBy private String createdBy;
+
+  @LastModifiedDate private LocalDateTime updatedAt;
+
+  @LastModifiedBy private String updatedBy;
+}
