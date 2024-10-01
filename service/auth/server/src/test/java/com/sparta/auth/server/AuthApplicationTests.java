3946927a1c4a6617a9e7ba29b4128b48f6302983
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-10-01 10:31:29 +0900

    Auth 모듈 생성 및 Gateway 필요없는 폴더 삭제 (#19)
    * remove(#17): Gateway gradle 삭제
    * chore(#17): Auth 모듈 생성

---
+++ service/auth/server/src/test/java/com/sparta/auth/server/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
