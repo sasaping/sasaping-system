4ecffa47c9ef6e445807c357f60dd54f019eb9b6
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-10-03 12:24:11 +0900

    사용자 이름으로 조회 기능 구현 및 user.dto 수정 (#37)
    * feat(#11): 로그인 시 필요한 사용자 이름으로 가져오는 API 구현
    - 테스트 코드 작성
    
    * fix(#11): user.dto 수정
    - user.dto에 들어가는 클래스는 다른 서비스에서 사용해야함으로 static으로 만들면 안됨
    - dto 수정에 따른 소스 코드 수정

--- service/user/server/src/main/java/com/sparta/user/application/service/UserService.java
+++ service/user/server/src/main/java/com/sparta/user/application/service/UserService.java
@@ -1,9 +1,11 @@
 package com.sparta.user.application.service;
 
 import static com.sparta.user.exception.UserErrorCode.USER_CONFLICT;
+import static com.sparta.user.exception.UserErrorCode.USER_NOT_FOUND;
 
 import com.sparta.user.domain.model.User;
 import com.sparta.user.domain.repository.UserRepository;
+import com.sparta.user.dto.infrastructure.UserDto;
 import com.sparta.user.exception.UserException;
 import com.sparta.user.presentation.request.UserRequest;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,17 @@ public class UserService {
             });
     // TODO: 비밀번호 암호화 필요(암호화 시 security 라이브러리가 필요한데 설치하면 테스트 불편함. 이후에 추가) - 경민
     userRepository.save(User.create(request, request.getPassword()));
+  }
+
+  public UserDto getUserByUsername(String username) {
+    User user = userRepository.findByUsername(username).orElseThrow(
+        () -> new UserException(USER_NOT_FOUND));
+    return new UserDto(
+        user.getId(),
+        user.getUsername(),
+        user.getPassword(),
+        user.getRole().name()
+    );
   }
 
 }
