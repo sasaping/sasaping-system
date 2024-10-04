ec7fd6f0afde27645c8417574b6de96559d1dbc5
Kim Jin Sun <122031650+kimzinsun@users.noreply.github.com>
2024-10-04 10:57:51 +0900

    슬랙 전송 api 생성 (#46)
    * feat(#30): 슬랙 모듈 생성
    
    * feat(#30): Message entity 생성
    
    * feat(#30): Message Exception 생성
    
    exception 발생시 에러 로깅
    
    * feat(#30): slack message 전송기능 생성
    
    * refactor(#30): message entity 수정
    
    - user Id 제거
    - base entity 상속 제거 및 send At 추가

---
+++ service/slack/server/src/main/java/com/sparta/slack/exception/MessageException.java
@@ -0,0 +1,12 @@
+package com.sparta.slack.exception;
+
+import com.sparta.common.domain.exception.BusinessException;
+
+
+public class MessageException extends BusinessException {
+
+  public MessageException(MessageErrorCode errorCode) {
+    super(errorCode.getStatus().name(), errorCode.getMessage());
+  }
+
+}
