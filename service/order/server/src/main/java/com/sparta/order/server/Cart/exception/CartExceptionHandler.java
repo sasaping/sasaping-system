493266d8226e076313456bf1bcdb373e69ba8111
linavell <linavell@naver.com>
2024-10-04 14:06:11 +0900

    fix(#42): @Valid 적용이 안되는 오류 해결

--- service/order/server/src/main/java/com/sparta/order/server/Cart/exception/CartExceptionHandler.java
+++ service/order/server/src/main/java/com/sparta/order/server/Cart/exception/CartExceptionHandler.java
@@ -4,6 +4,7 @@ import com.sparta.common.domain.exception.BusinessException;
 import com.sparta.common.domain.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,11 +14,20 @@ public class CartExceptionHandler {
 
   @ExceptionHandler(BusinessException.class)
   public ApiResponse<Void> handleBusinessException(BusinessException ex) {
+    log.error(ex.getStatusName(), ex.getMessage());
     return ApiResponse.error(ex.getStatusName(), ex.getMessage());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ApiResponse<Void> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    ex.printStackTrace();
+    String message = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+    return ApiResponse.error(HttpStatus.BAD_REQUEST.name(), message);
   }
 
   @ExceptionHandler(Exception.class)
   public ApiResponse<Void> handleAllExceptions(Exception ex) {
+    ex.printStackTrace();
     return ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.name(), ex.getMessage());
   }
 
