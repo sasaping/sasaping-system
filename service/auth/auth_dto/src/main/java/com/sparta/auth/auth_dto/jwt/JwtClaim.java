62f385f3fb298c0851d44bec78585f5fe40912a2
kyeonkim <kyeonkim5021@gmail.com>
2024-10-07 12:34:35 +0900

    refactor(#15): merge 후 경로 수정

--- service/auth/auth_dto/src/main/java/com/sparta/auth/auth_dto/jwt/JwtClaim.java
+++ service/auth/auth_dto/src/main/java/com/sparta/auth/auth_dto/jwt/JwtClaim.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:service/auth/dto/src/main/java/com/sparta/auth/dto/jwt/JwtClaim.java
-package com.sparta.auth.dto.jwt;
-========
 package com.sparta.auth.auth_dto.jwt;
->>>>>>>> 90d8e7d67d42838f7c8951951d3ad01da690273c:service/auth/auth_dto/src/main/java/com/sparta/auth/auth_dto/jwt/JwtClaim.java
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
