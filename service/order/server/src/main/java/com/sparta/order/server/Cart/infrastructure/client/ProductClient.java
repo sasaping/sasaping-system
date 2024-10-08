2ab42b754de7534586da7abc0497d2e7a5c0da0f
linavell <linavell@naver.com>
2024-10-08 15:00:40 +0900

    feat(#60): 장바구니에 필요한 상품 서비스 FeignClient 설정 추가

---
+++ service/order/server/src/main/java/com/sparta/order/server/Cart/infrastructure/client/ProductClient.java
@@ -0,0 +1,15 @@
+package com.sparta.order.server.Cart.infrastructure.client;
+
+import com.sparta.product_dto.ProductDto;
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "product")
+public interface ProductClient {
+
+  @GetMapping("/internal/products")
+  List<ProductDto> getProductList(@RequestParam(name = "productIds") List<String> productIds);
+
+}
