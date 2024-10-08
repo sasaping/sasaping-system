4d32e2e8170ff1125583c161977dccac48e21bb6
linavell <linavell@naver.com>
2024-10-08 15:01:36 +0900

    feat(#60): 상품 조회 내부 API 로직 변경

--- service/product/product_server/src/main/java/com/sparta/product/presentation/controller/ProductInternalController.java
+++ service/product/product_server/src/main/java/com/sparta/product/presentation/controller/ProductInternalController.java
@@ -2,22 +2,24 @@ package com.sparta.product.presentation.controller;
 
 import com.sparta.product.application.ProductService;
 import com.sparta.product_dto.ProductDto;
-import com.sparta.product_dto.ProductReadRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/internal/products")
 public class ProductInternalController {
+
   private final ProductService productService;
 
   @GetMapping
-  public List<ProductDto> getProductList(@RequestBody ProductReadRequest request) {
-    return productService.getProductList(request.productIds());
+  public List<ProductDto> getProductList(
+      @RequestParam(name = "productIds") List<String> productIds) {
+    return productService.getProductList(productIds);
   }
+
 }
