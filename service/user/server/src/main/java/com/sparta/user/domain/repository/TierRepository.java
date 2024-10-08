b113cbf452067de8ffc5355e94a9b64d813cc26b
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-10-08 17:18:49 +0900

    등급 CRUD 기능 구현 (#73)
    * feat(#41): 등급 생성, 조회, 수정, 삭제
    - 단위, API 테스트 작성

---
+++ service/user/server/src/main/java/com/sparta/user/domain/repository/TierRepository.java
@@ -0,0 +1,21 @@
+package com.sparta.user.domain.repository;
+
+import com.sparta.user.domain.model.Tier;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TierRepository {
+
+  Tier save(Tier tier);
+
+  Optional<Tier> findByName(String name);
+
+  List<Tier> findAll();
+
+  Optional<Tier> findById(Long tierId);
+
+  void delete(Tier tier);
+
+}
--- service/user/server/src/main/java/com/sparta/user/exception/UserErrorCode.java
+++ service/user/server/src/main/java/com/sparta/user/exception/UserErrorCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserErrorCode {
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-  USER_CONFLICT(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다.");
+  USER_CONFLICT(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다."),
+
+  TIER_CONFLICT(HttpStatus.CONFLICT, "이미 존재하는 등급입니다."),
+  TIER_NOT_FOUND(HttpStatus.NOT_FOUND, "등급을 찾을 수 없습니다.");
 
   private final HttpStatus status;
   private final String message;
