0b9b500e4bd15605d39699b8739e9b47b433a921
kyeonkim <kyeonkim5021@gmail.com>
2024-10-09 19:47:27 +0900

    fix(#72): point request > user_dto에 pointHistoryDto로 수정 - 내부 메서드인데 외부 메서드처럼 만들어버림

--- service/user/server/src/main/java/com/sparta/user/presentation/controller/UserInternalController.java
+++ service/user/server/src/main/java/com/sparta/user/presentation/controller/UserInternalController.java
@@ -1,9 +1,13 @@
 package com.sparta.user.presentation.controller;
 
+import com.sparta.user.application.service.PointHistoryService;
 import com.sparta.user.application.service.UserService;
+import com.sparta.user.user_dto.infrastructure.PointHistoryDto;
 import com.sparta.user.user_dto.infrastructure.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserInternalController {
 
   private final UserService userService;
+  private final PointHistoryService pointHistoryService;
 
   @GetMapping
   public UserDto getUserByUsername(@RequestParam(value = "username") String username) {
     return userService.getUserByUsername(username);
   }
+
+  @PostMapping("/point")
+  public void createPointHistory(@RequestBody PointHistoryDto request) {
+    pointHistoryService.createPointHistory(request);
+  }
+
 }
