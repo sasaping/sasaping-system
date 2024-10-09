552e116cb9356ec429e84fced3fb19183b7c30aa
harper <35358294+yooyouny@users.noreply.github.com>
2024-10-09 17:19:06 +0900

    rename(#59): 서비스명 ElasticsearchService로 수정

--- service/product/product_server/src/main/java/com/sparta/product/application/ElasticSearchService.java
+++ service/product/product_server/src/main/java/com/sparta/product/application/ElasticSearchService.java
@@ -1,18 +1,26 @@
 package com.sparta.product.application;
 
+import com.sparta.product.domain.model.SortOption;
+import com.sparta.product.domain.repository.ElasticSearchRepository;
+import com.sparta.product.domain.repository.ElasticsearchCustomRepository;
 import com.sparta.product.infrastructure.elasticsearch.dto.ProductSearchDto;
 import com.sparta.product.presentation.response.ProductResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j(topic = "ElasticSearchService")
-public class ElasticSearchService {
-  private final ElasticsearchRepository elasticsearchRepository;
+@Slf4j(topic = "ElasticsearchService")
+public class ElasticsearchService {
+  private final ElasticSearchRepository elasticsearchRepository;
+  private final ElasticsearchCustomRepository customRepository;
 
   @Async
   public void saveProduct(ProductResponse response) {
@@ -33,5 +41,21 @@ public class ElasticSearchService {
     ProductSearchDto product = ProductSearchDto.toDto(response);
     elasticsearchRepository.delete(product);
     log.info("delete product in elastic search {}", product.getProductId());
+  }
+
+  public Page<ProductSearchDto> getProductList(
+      int page,
+      int size,
+      Long categoryId,
+      String brandName,
+      Long minPrice,
+      Long maxPrice,
+      String productSize,
+      String mainColor,
+      String sortOption) throws IOException {
+    SortOption sort = SortOption.valueOf(sortOption.toUpperCase());
+    Pageable pageable = PageRequest.of(page, size, Sort.by(sort.getField()));
+    return customRepository.searchProductList(
+        categoryId, brandName, minPrice, maxPrice, productSize, mainColor, page, size, pageable, sort);
   }
 }
