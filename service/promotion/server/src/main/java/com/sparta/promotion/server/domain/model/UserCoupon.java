f59457fe6597feff77d14b1f4358217e748ac6ff
kyeonkim <kyeonkim5021@gmail.com>
2024-10-09 23:34:16 +0900

    feat(#81): 유저 쿠폰 Entity 생성 - 유저에 둘 지, 프로모션에 둘 지 고민이라 관계를 맺지 않음

---
+++ service/promotion/server/src/main/java/com/sparta/promotion/server/domain/model/UserCoupon.java
@@ -0,0 +1,37 @@
+package com.sparta.promotion.server.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "p_user_coupon")
+@Entity
+public class UserCoupon {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_coupon_id")
+  private Long id;
+
+  @Column(nullable = false)
+  private Long userId;
+
+  @Column(nullable = false)
+  private Long couponId;
+
+  @Column(nullable = false)
+  private Boolean isUsed;
+
+}
