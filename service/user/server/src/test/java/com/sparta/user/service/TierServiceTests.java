266acae1eacfc9ea89aebd901ab096496c5b8fc9
kyeonkim <kyeonkim5021@gmail.com>
2024-10-13 23:55:08 +0900

    rename(#13): 등급 테스트 코드 파일 이름 변경 후 파일 이동

--- service/user/server/src/test/java/com/sparta/user/service/TierServiceTests.java
+++ service/user/server/src/test/java/com/sparta/user/service/TierServiceTests.java
@@ -1,4 +1,4 @@
-package com.sparta.user;
+package com.sparta.user.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -26,7 +26,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
-public class TierApplicationTests {
+public class TierServiceTests {
 
   @MockBean
   private TierRepository tierRepository;
