c96bc4682bf6987ff74e3c37655998c43a8e7cb1
Kim Jin Sun <122031650+kimzinsun@users.noreply.github.com>
2024-10-14 15:18:15 +0900

    elastic search 검색엔진 (#105)
    * feat(#76): search 모듈 생성
    
    * feat(#76): keyword search 기능 생성
    
    * feat(#76): 검색 필터링 추가
    
    * feat(#76): exception 처리 로직 추가
    
    * fix(#76): keyword 조회시 전체 검색되는 에러 수정
    
    * feat(#76): keyword field 추가, size filter 추가
    
    * feat(#76): sortOption 추가
    
    * feat(#76): 정렬 조건에 리뷰순 추가

---
+++ service/search/server/src/main/java/com/sparta/server/application/SearchService.java
@@ -0,0 +1,78 @@
+package com.sparta.server.application;
+
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.RangeQuery;
+import co.elastic.clients.json.JsonData;
+import com.sparta.server.domain.ProductSearchDto;
+import com.sparta.server.domain.SortOption;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SearchService {
+
+  private final ElasticsearchOperations elasticsearchOperations;
+
+  public SearchHits<ProductSearchDto> searchProducts(String keyword, Long categoryId,
+      String brandName, String mainColor, Double minPrice, Double maxPrice, String size,
+      SortOption sortOption, int page,
+      int pageSize) {
+    BoolQuery.Builder boolQuery = new BoolQuery.Builder();
+
+    boolQuery.must(q -> q
+        .multiMatch(m -> m
+            .query(keyword)
+            .fields("productName", "brandName", "description", "tags")
+        )
+    );
+
+    if (categoryId != null) {
+      boolQuery.filter(q -> q.term(t -> t.field("categoryId").value(categoryId)));
+    }
+
+    if (brandName != null && !brandName.isEmpty()) {
+      boolQuery.filter(q -> q.term(t -> t.field("brandName.keyword").value(brandName)));
+    }
+
+    if (mainColor != null && !mainColor.isEmpty()) {
+      boolQuery.filter(q -> q.term(t -> t.field("mainColor.keyword").value(mainColor)));
+    }
+
+    if (size != null && !size.isEmpty()) {
+      boolQuery.filter(q -> q.term(t -> t.field("size.keyword").value(size)));
+    }
+
+    if (minPrice != null || maxPrice != null) {
+      RangeQuery.Builder rangeQuery = new RangeQuery.Builder().field("discountedPrice");
+      if (minPrice != null) {
+        rangeQuery.gte(JsonData.of(minPrice));
+      }
+      if (maxPrice != null) {
+        rangeQuery.lte(JsonData.of(maxPrice));
+      }
+      boolQuery.filter(q -> q.range(rangeQuery.build()));
+    }
+
+    NativeQuery searchQuery = NativeQuery.builder()
+        .withQuery(boolQuery.build()._toQuery())
+        .withPageable(PageRequest.of(page, pageSize))
+        .build();
+
+    if (sortOption != null && !sortOption.equals(SortOption.RELEVANCE)) {
+      Sort sort = sortOption.getSort();
+      searchQuery.setSort(sort);
+    }
+
+    return elasticsearchOperations.search(searchQuery, ProductSearchDto.class);
+  }
+
+
+}
