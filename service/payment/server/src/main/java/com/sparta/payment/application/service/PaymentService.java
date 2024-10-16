1b504b4683eaf921308b90db1250ea10153617be
Kim JinSeon <kwlstjs00@gmail.com>
2024-10-16 15:47:40 +0900

    fix: user Id 누락된 값 추가

--- service/payment/server/src/main/java/com/sparta/payment/application/service/PaymentService.java
+++ service/payment/server/src/main/java/com/sparta/payment/application/service/PaymentService.java
@@ -171,6 +171,7 @@ public class PaymentService {
         .paymentId(payment.getPaymentId())
         .orderId(payment.getOrderId())
         .amount(payment.getAmount())
+        .userId(payment.getUserId())
         .success(false)
         .build();
 
