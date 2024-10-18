c3a40ea5c01b6ce4e04b877411c07a243e817a49
kyeonkim <kyeonkim5021@gmail.com>
2024-10-18 12:18:46 +0900

    feat(#74): 특정 사용자 등급 조회 API

--- service/user/server/src/main/java/com/sparta/user/presentation/controller/UserTierController.java
+++ service/user/server/src/main/java/com/sparta/user/presentation/controller/UserTierController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,14 @@ public class UserTierController {
   ) {
     return ApiResponse.ok(userService.getUserTierByUserId(claim.getUserId()));
   }
+
+  @PreAuthorize("hasAnyRole('ROLE_MANAGER', 'ROLE_ADMIN')")
+  @GetMapping("/{userId}")
+  public ApiResponse<UserTierResponse.Get> getUserTierByUserId(
+      @PathVariable Long userId
+  ) {
+    return ApiResponse.ok(userService.getUserTierByUserId(userId));
+  }
+
 
 }
