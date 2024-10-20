6346f24c66fd91f34511455257011ec233076285
kyeonkim <kyeonkim5021@gmail.com>
2024-10-20 19:31:03 +0900

    feat(#148): config server 모듈 생성

---
+++ service/config/server/src/main/java/com/sparta/config/server/ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigApplication.class, args);
+  }
+
+}
