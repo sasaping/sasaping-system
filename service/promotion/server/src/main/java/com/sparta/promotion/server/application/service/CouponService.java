b38d78908e82b1a222177c6831bcb786e69b76c2
kyeonkim <kyeonkim5021@gmail.com>
2024-10-21 17:42:37 +0900

    feat(#81): 쿠폰 생성 시 이벤트 유무 검사

--- service/promotion/server/src/main/java/com/sparta/promotion/server/application/service/CouponService.java
+++ service/promotion/server/src/main/java/com/sparta/promotion/server/application/service/CouponService.java
@@ -4,6 +4,7 @@ import com.sparta.common.domain.entity.KafkaTopicConstant;
 import com.sparta.promotion.server.domain.model.Coupon;
 import com.sparta.promotion.server.domain.model.UserCoupon;
 import com.sparta.promotion.server.domain.repository.CouponRepository;
+import com.sparta.promotion.server.domain.repository.EventRepository;
 import com.sparta.promotion.server.domain.repository.UserCouponRepository;
 import com.sparta.promotion.server.exception.PromotionErrorCode;
 import com.sparta.promotion.server.exception.PromotionException;
@@ -23,19 +24,17 @@ public class CouponService {
 
   private final CouponRepository couponRepository;
   private final UserCouponRepository userCouponRepository;
+  private final EventRepository eventRepository;
   private final KafkaTemplate<String, CouponRequest.Event> kafkaTemplate;
 
-  // TODO(경민): 이벤트 테이블을 만들어서 이벤트 ID가 들어올 경우, 해당 이벤트가 있는지 검사해야함.
   @Transactional
   public void createEventCoupon(CouponRequest.Create request) {
-    /* 이벤트 검사 코드
     if (request.getEventId() == null) {
       throw new PromotionException(PromotionErrorCode.EVENT_NOT_FOUND);
     }
     eventRepository
         .findById(request.getEventId())
         .orElseThrow(() -> new PromotionException(PromotionErrorCode.EVENT_NOT_FOUND));
-    */
     couponRepository.save(Coupon.create(request));
   }
 
