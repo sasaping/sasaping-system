65bc6c1e6b31a6fde53a7563002404f23160dd33
linavell <linavell@naver.com>
2024-11-05 13:17:59 +0900

    feat(#152): 알림 dto 모듈 생성

---
+++ service/notification/notification_dto/src/main/java/com/sparta/notification/notification_dto/NotificationDtoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.notification.notification_dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationDtoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NotificationDtoApplication.class, args);
+  }
+
+}
